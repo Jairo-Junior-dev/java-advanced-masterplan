36a819dcd49d1307fe5cbcbc62ddcd666d749c1d
Jairo Pinheiro <jairopnhr02@gmail.com>
2025-07-27 19:21:04 -0300

    doc: Comments in class Funcionario

--- src/main/java/com/minhaempresa/design_patterns_solid/solid/srp/Funcionario.java
+++ src/main/java/com/minhaempresa/design_patterns_solid/solid/srp/Funcionario.java
@@ -3,15 +3,18 @@ package com.minhaempresa.design_patterns_solid.solid.srp;
 public class Funcionario {
     private String name;
     private double salario;
+    // Funcionalidade unica de adicionar nome e salario.
     public void setarFuncionario( String nome, double salario){
 
         this.salario = salario;
         this.name = nome;
     }
 
+    //Funcionalidade Unica de calcular o salario.
     public void calcularSalario(){
           this.salario  = this.salario *  1.3;
     }
+    //Funcionalidade unica de mostrar as informações do funcionario.
     public void mostrarInfor(){
         System.out.println("Nome do funcionario:  " + this.name + "\n Salario do funcionario: " + this.salario);
     }
